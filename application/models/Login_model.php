2a957ef10138043a65af9cc93231fa7fa8f5ec54
Mauricio <mauricioq17@gmail.com>
2015-10-06 01:19:24 -0300

    Se agrega funcionalidad para autenticar usuario comparando la contraseña ingresada contra el hash guardado

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -8,6 +8,18 @@ class Login_model extends CI_Model
 
     public function autenticar_usuario($datos)
     {
-    	
+    	$resultado_consulta = $this->db->query('SELECT email, clave FROM usuario WHERE email = "' . $datos['email'] . '" AND fecha_baja IS NULL');
+
+    	if ($resultado_consulta->num_rows() > 0)
+    	{
+    		$clave = $resultado_consulta ->row()->clave;
+
+    		if (password_verify($datos['clave'], $clave))
+    		{
+    			return TRUE;
+    		}
+    	}
+
+    	return FALSE;
     }
 }
