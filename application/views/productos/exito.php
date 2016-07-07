8d33b2610c89507b295d2f34fec0a583602c9e6b
Mauricio Quiroga <mauricioq17@gmail.com>
2016-07-07 13:28:50 -0300

    Corrección de mensaje

--- application/views/productos/exito.php
+++ application/views/productos/exito.php
@@ -4,5 +4,5 @@
 	<?php echo $mensaje ?>
 	<a href="<?php echo site_url('productos/nuevo') ?>">Nuevo producto</a>
 	<br>
-	<a href="<?php echo site_url('productos/ver') ?>">Administrar de productos</a>
+	<a href="<?php echo site_url('productos/ver') ?>">Administrar productos</a>
 </div>
